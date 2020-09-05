1e8bec3386a9824252393262f65495b15a7fa517
laiananardi <lailai.santiago@gmail.com>
2020-09-04 22:48:48 -0300

    day11

--- projects/jscursoemvideo/repetições/exercicio/script.js
+++ projects/jscursoemvideo/repetições/exercicio/script.js
@@ -1,31 +1,45 @@
-function count (){
-    const beg = document.getElementById('beggining')
-    const end = document.getElementById('end')
-    const pat = document.getElementById('pattern')
-    const ans = document.getElementById('ans')
+// function count (){
+//     const beg = document.getElementById('beggining')
+//     const end = document.getElementById('end')
+//     const pat = document.getElementById('pattern')
+//     const ans = document.getElementById('ans')
 
-    if(beg.value.length == 0 || end.value.length == 0 || pat.value.length == 0){
-        //se não preencher algum valor
-        window.alert('Faltam dados!')
-    }else{
-        ans.innerHTML = `contando: <br>`
-        const b = Number(beg.value)
-        const e = Number(end.value)
-        const p = Number(pat.value)
-        if (p <= 0) {
-            window.alert('Passo inválido! Consideranod passo = 1')
-            p = 1
-        }
-        if (b < e) {
-            //contagem crescente
-            for(let c = b; c <= e; c += p){
-                ans.innerHTML += `${c} `
-            }
-        }else{
-            //contagem decrescente
-            for(let c = b; c >= e; c -= p){
-                ans.innerHTML += `${c} `
-            }
-        }
-    }
+//     if(beg.value.length == 0 || end.value.length == 0 || pat.value.length == 0){
+//         //se não preencher algum valor
+//         window.alert('Faltam dados!')
+//     }else{
+//         ans.innerHTML = `contando: <br>`
+//         const b = Number(beg.value)
+//         const e = Number(end.value)
+//         const p = Number(pat.value)
+//         if (p <= 0) {
+//             window.alert('Passo inválido! Consideranod passo = 1')
+//             p = 1
+//         }
+//         if (b < e) {
+//             //contagem crescente
+//             for(let c = b; c <= e; c += p){
+//                 ans.innerHTML += `${c} `
+//             }
+//         }else{
+//             //contagem decrescente
+//             for(let c = b; c >= e; c -= p){
+//                 ans.innerHTML += `${c} `
+//             }
+//         }
+//     }
+// }
+const inicio = 0
+const final = 10
+const passo = 2
+
+// for(let c = inicio; c <= final; c += passo){
+//     console.log(c)
+// }     
+
+
+while(inicio <= final){
+    let c = inicio 
+    c += passo
+    console.log(c)
 }
