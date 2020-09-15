e27fa1b06cb33088ac14e8b632445a952f48fba5
laiananardi <54119801+laiananardi@users.noreply.github.com>
2020-09-15 14:27:33 -0300

    day23

--- projects/text/script.js
+++ projects/text/script.js
@@ -1,19 +1,37 @@
 document.addEventListener('DOMContentLoaded', init);
 
-        var i = 0;//start point
-        var txt = ['Olá, mundo!', 'Hello, world!', 'Bonjour le monde!']
-        var text = document.getElementById('text')
-        var time = 1500;
-        function init(){
-            
-            text.innerText= `${txt[i]}`
-            
-            if(i < txt.length -1){
-                i++;
-            } else{
-                i = 0;
-            }
+var i = 0;//start point
+// TEXT CHANGING to use on the mobile form
+// var txt2 = []
+// var txt3 = []
 
-            setTimeout("init()", time);   
-        }
-        
+var text2 = document.getElementsByTagName('h2')[0]
+var text3 = document.getElementsByTagName('h3')[0]
+var img = []
+var time = 3000;
+
+img.push('img/img1.png')
+img.push('img/img2.png')
+
+
+// txt2.push('Fácil e Ágil')
+// txt2.push('Cuidados para pet')
+// txt2.push('Atendimento personalizado')
+
+// txt3.push('Encontramos o veterinário mais próximo de você.')
+// txt3.push('Atendimento Veterinário, Banho e Tosa e Adestramento.')
+// txt3.push('O veterinário vai até sua casa.')
+function init(){
+    
+    // text2.innerText= `${txt2[i]}`
+    // text3.innerText= `${txt3[i]}`
+    document.image.src = img[i]
+            
+    if(i < img.length -1){
+    	i++;
+    } else{
+    	i = 0;
+    }
+
+    setTimeout("init()", time);   
+}
