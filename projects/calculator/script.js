6f503d21a9802eb6d2495c939c675b0731a8f4b9
laiananardi <54119801+laiananardi@users.noreply.github.com>
2020-09-17 22:29:06 -0300

    calculator

---
+++ projects/calculator/script.js
@@ -0,0 +1,107 @@
+
+
+// Shortcut to get elements
+var el = function (element) {
+  if (element.charAt(0) === "#") {
+    return document.querySelector(element);
+  }
+
+  return document.querySelectorAll(element);
+};
+
+var viewer = el("#viewer"),
+  equals = el("#equals"),
+  nums = el(".num"),
+  ops = el(".ops"),
+  theNum = "",
+  oldNum = "",
+  resultNum,
+  operator;
+
+var setNum = function () {
+  if (resultNum) {
+    theNum = this.getAttribute("data-num");
+    resultNum = "";
+  } else {
+    theNum += this.getAttribute("data-num");
+  }
+
+  viewer.innerHTML = theNum;
+
+};
+
+var moveNum = function () {
+  oldNum = theNum;
+  theNum = "";
+  operator = this.getAttribute("data-ops");
+
+  equals.setAttribute("data-result", "");
+};
+
+var displayNum = function () {
+
+  oldNum = parseFloat(oldNum);
+  theNum = parseFloat(theNum);
+
+  switch (operator) {
+    case "plus":
+      resultNum = oldNum + theNum;
+      break;
+
+    case "minus":
+      resultNum = oldNum - theNum;
+      break;
+
+    case "times":
+      resultNum = oldNum * theNum;
+      break;
+
+    case "divided by":
+      resultNum = oldNum / theNum;
+      break;
+
+    default:
+      resultNum = theNum;
+  }
+
+
+  if (!isFinite(resultNum)) {
+    if (isNaN(resultNum)) {
+      //if it's NaN
+      resultNum = "something wrong!"
+    } else {
+      //if it's infinity
+      resultNum = "impossible!"
+    }
+  }
+
+  viewer.innerHTML = resultNum;
+  equals.setAttribute("data-result", resultNum);
+
+  oldNum = 0;
+  theNum = resultNum;
+
+};
+
+var clearAll = function () {
+  oldNum = "";
+  theNum = "";
+  viewer.innerHTML = "0";
+  equals.setAttribute("data-result", resultNum);
+};
+
+//Click events on JS code
+for (var i = 0, l = nums.length; i < l; i++) {
+  nums[i].onclick = setNum;
+}
+
+for (var i = 0, l = ops.length; i < l; i++) {
+  ops[i].onclick = moveNum;
+}
+
+equals.onclick = displayNum;
+
+el("#clear").onclick = clearAll;
+
+
+
