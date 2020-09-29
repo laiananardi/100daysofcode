2f8b6f42501d9118c53cc3ebfda30fa879683815
laiananardi <54119801+laiananardi@users.noreply.github.com>
2020-09-29 15:03:35 -0300

    challenge02

--- courses/curso-javascript-ninja/challenge-02/script.js
+++ courses/curso-javascript-ninja/challenge-02/script.js
@@ -1,13 +1,23 @@
 // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
 
+function soma (num1, num2){
+    return num1 + num2
+}
 
-// Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
+// Declare uma variável que receba a invocação da função criada acima, 
+// passando dois números quaisquer por argumento, e somando `5` ao resultado 
+// retornado da função.
 
-
+const somar = soma(2,3) + 5
+ 
 // Qual o valor atualizado dessa variável?
 
+console.log(somar)
+//output: 10
 
 // Declare uma nova variável, sem valor.
+
+const variable
 
 /*
 Crie uma função que adicione um valor à variável criada acima, e retorne a string:
@@ -15,11 +25,19 @@ Crie uma função que adicione um valor à variável criada acima, e retorne a s
 Onde VALOR é o novo valor da variável.
 */
 
+function value (){
+    variable = 5
+    return `O valor da variável agora é ${variable}`
+}
+
+
 // Invoque a função criada acima.
 
+value()
 
 // Qual o retorno da função? (Use comentários de bloco).
 
+/* 'O valor da variável agora é 5' */
 
 /*
 Crie uma função com as seguintes características:
@@ -29,17 +47,28 @@ Crie uma função com as seguintes características:
 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
 */
 
-
+function check (num1, num2, num3){
+    if(num1 === undefined || num2 === undefined || num3 === undefined){
+        return 'Preencha todos os campos corretamente !'
+    }else{
+        return (num1 * num2 * num3) + 2
+    }
+}
 // Invoque a função criada acima, passando só dois números como argumento.
 
+check(2,3)
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
 
+// 'Preencha todos os campos corretamente !'
 
 // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
 
+check(1,2,3)
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+
+// 8
 
 /*
 Crie uma função com as seguintes características:
@@ -51,6 +80,26 @@ Crie uma função com as seguintes características:
 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
 */
 
-
+function checkArg(num1, num2, num3){
+    if (num1 !== undefined && num2 === undefined && num3 === undefined){
+        return num1
+        }else if (num1 !== undefined && num2 !== undefined && num3 === undefined){
+            return num1 + num2
+        }else if (num1 !== undefined && num2 !== undefined && num3 !== undefined){
+            return (num1 + num2)/num3
+        }else if(num1 === undefined && num2 === undefined && num3 ===undefined){
+            return false
+        }else{
+            return null
+        }
+}
 // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
 
+checkArg(2)
+//2
+checkArg(2,3)
+//5
+checkArg(5,10,3)
+//5
+checkArg()
+//false
