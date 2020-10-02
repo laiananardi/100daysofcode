b0383163203bc6c0c7864f30d04721e49f5814d9
laiananardi <54119801+laiananardi@users.noreply.github.com>
2020-10-02 16:48:13 -0300

    satisfaction

--- projects/star_rating/script.js
+++ projects/star_rating/script.js
@@ -1,47 +1,80 @@
-const five = document.getElementById('five')
-const four = document.getElementById('four')
-const three = document.getElementById('three')
-const two = document.getElementById('two')
-const one = document.getElementById('one')
+const satisfactions = document.getElementsByClassName('satisfactions')[0]
+
 
 const list = [0, 1, 2, 3, 4]
 var stars = JSON.parse(localStorage.getItem('stars')) || [];
+var star = document.getElementById(stars).className
+
 
 list.forEach(function (element) {
 
     document.getElementById(element).addEventListener("click", function () {
         stars = []
-        var cls = document.getElementById(element).className
         stars.push(element)
         localStorage.setItem('stars', JSON.stringify(stars))
+        star = document.getElementById(element).className
+    
+        
+        showStars()
+    
 
-
-        if (cls.includes("unchecked")) {
-            for (i = 0; i <= element; i++) {
-
-                document.getElementById(i).classList.remove("unchecked")
-                document.getElementById(i).classList.add("checked")
-            }
-        } else {
-            for (i = 4; i > element; i--) {
-
-                document.getElementById(i).classList.remove("checked")
-                document.getElementById(i).classList.add("unchecked")
-            }
-        }
     })
 
 })
 
+function showSatisfactions() {
+    //satisfactions
+    if (stars == 4) {
+
+        satisfactions.innerHTML = `<div class="satisfaction" id="five">
+                                        <p>Ótimo!</p>
+                                        <i class="fas fa-laugh-squint"></i>
+                                    </div>`
+    
+
+
+    } else if (stars == 3) {
+
+        satisfactions.innerHTML = `<div class="satisfaction" id="four">
+                                        <p>Bom!</p>
+                                        <i class="fas fa-smile-beam"></i>
+                                    </div>`
+       
+
+    } else if (stars == 2) {
+
+        satisfactions.innerHTML = `<div class="satisfaction" id="three">
+                                        <p>Regular!</p>
+                                        <i class="fas fa-meh"></i>
+                                    </div>`
+
+    } else if (stars == 1) {
+
+        satisfactions.innerHTML = `<div class="satisfaction" id="two">
+                                        <p>Ruim!</p>
+                                        <i class="fas fa-frown"></i>
+                                    </div>`
+
+    } else {
+
+        satisfactions.innerHTML = `<div class="satisfaction" id="one">
+                                        <p>Péssimo!</p>
+                                        <i class="fas fa-angry"></i>
+                                    </div>`
+
+    }
+
+}
+
+
 function showStars() {
-
-    var star = document.getElementById(stars).className
-
+    
     if (star.includes("unchecked")) {
         for (i = 0; i <= stars; i++) {
-
-            document.getElementById(i).classList.remove("unchecked")
+            
             document.getElementById(i).classList.add("checked")
+            document.getElementById(i).classList.remove("unchecked")
+            
         }
     } else {
         for (i = 4; i > stars; i--) {
@@ -50,6 +83,6 @@ function showStars() {
             document.getElementById(i).classList.add("unchecked")
         }
     }
-
+    showSatisfactions()
 
 }
