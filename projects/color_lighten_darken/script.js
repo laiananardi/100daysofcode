e77bd01b89020b77d089d807575f4af5d2d9ff5e
laiananardi <54119801+laiananardi@users.noreply.github.com>
2020-11-04 17:33:05 -0300

    button

--- projects/color_lighten_darken/script.js
+++ projects/color_lighten_darken/script.js
@@ -1,7 +1,27 @@
 const hexInput = document.getElementById('hexInput');
 const inputColor = document.getElementById('inputColor');
+const alteredColor = document.getElementById('alteredColor');
+const alteredColorText = document.getElementById('alteredColorText');
 const sliderText = document.getElementById('sliderText');
 const slider = document.getElementById('slider');
+const lightenText = document.getElementById('lightenText');
+const darkenText = document.getElementById('darkenText');
+const toggleBtn = document.getElementById('toggleBtn');
+
+
+toggleBtn.addEventListener('click', () => {
+  if(toggleBtn.classList.contains('toggled')){
+    toggleBtn.classList.remove('toggled');
+    lightenText.classList.remove('unselected');
+    darkenText.classList.add('unselected');
+  } else {
+    toggleBtn.classList.add('toggled');
+    lightenText.classList.add('unselected');
+    darkenText.classList.remove('unselected');
+  } 
+  reset(); 
+})
+
 
 hexInput.addEventListener('keyup', () => {
   
@@ -10,7 +30,8 @@ hexInput.addEventListener('keyup', () => {
   
   const strippedHex = hex.replace('#', '');
 
-  inputColor.style.backgroundColor = "#" + strippedHex;  
+  inputColor.style.backgroundColor = "#" + strippedHex; 
+  reset();  
 })
 
 const isValidHex = (hex) => {
@@ -52,13 +73,42 @@ const alterColor = (hex, percentage) => {
   
   const amount = Math.floor((percentage/100) * 255);
   
-  const newR = r + amount;
-  const newG = g + amount;
-  const newB = b + amount;
+  const newR = increaseWithin0To255(r,amount);
+  const newG = increaseWithin0To255(g,amount);
+  const newB = increaseWithin0To255(b,amount);
+  
   console.log({newR, newG, newB});
   return convertRGBToHex(newR, newG, newB);
 }
 
+const increaseWithin0To255 = (hex, amount) => {
+  // const newHex = hex + amount;
+  // if(newHex > 255) return 255;
+  // if(newHex < 0) return 0;
+  // return newHex;
+  return Math.min(255, Math.max(0, hex + amount));
+}
+
 slider.addEventListener('input', () => {
+  if(!isValidHex(hexInput.value)) return;
+
   sliderText.textContent = `${slider.value}%`;
+
+  const valueAddition  = 
+  toggleBtn.classList.contains('toggled') ? 
+  -slider.value 
+  : slider.value;
+
+
+  const alteredHex = alterColor(hexInput.value, valueAddition);
+  alteredColor.style.backgroundColor = alteredHex;
+  alteredColorText.innerText = `Altered Color ${alteredHex}`;
+  
 })
+
+const reset = () =>{ 
+  slider.value = 0;
+  sliderText.innerText=`0%`;
+  alteredColor.style.backgroundColor = hexInput.value;
+  alteredColorText.innerText = `Altered Color ${hexInput.value}`; 
+}
