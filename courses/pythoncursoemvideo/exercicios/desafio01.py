8b768409ebbffa2e821a2f5c6c5c6a9937b695be
laiananardi <lailai.santiago@gmail.com>
2020-11-20 22:57:37 -0300

    exercicios 01,02,03

---
+++ courses/pythoncursoemvideo/exercicios/desafio01.py
@@ -0,0 +1,6 @@
+# Criar um script Python que leia o nome de uma pessoa e mostra uma 
+# mensagem de boas=vindas de acordo com o valor digitado.
+
+print("Qual o seu nome?")
+nome = input()
+print("Olá, "+nome+"! Prazer em te conhecer.")
