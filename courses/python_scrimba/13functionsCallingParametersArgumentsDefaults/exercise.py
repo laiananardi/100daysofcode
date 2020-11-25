8a26080e0f2500883d6a48867956806c120fb8f7
laiananardi <lailai.santiago@gmail.com>
2020-11-25 18:54:29 -0300

    functions, named notations, return

--- courses/python_scrimba/13functionsCallingParametersArgumentsDefaults/exercise.py
+++ courses/python_scrimba/13functionsCallingParametersArgumentsDefaults/exercise.py
@@ -1,20 +1,23 @@
-def mensagem(nome, idade=25):
+def mensagem(nome, idade=25, cor='red'):
     #Mensagem com o nome e a idade que o usuario inserir, utilizando idade=28 como default.
-    print('Olá '  +  nome + ', you are ' + str(idade) +'!')
-    print(f'Hello {nome}, you are {idade}!')
+    # print('Olá '  +  nome.capitalize() + ', você vai fazer ' + str(idade+1) +' anos no próximo aniversário!')
+    print(f'Olá {nome.capitalize()}, você vai fazer {idade +1} anos no próximo aniversário!')
+    print(f'Você gosta da cor {cor.lower()}!')
 
 print('Seu nome: ')
 nome = input()
 print('Sua idade: ')
 idade = input()
-mensagem(nome, idade)
+print('Sua cor favorita:')
+cor = input()
+mensagem(nome, int(idade), cor)
 
 
-# 1. Add new print statement - on a new line
-#    which says 'We hear you like the color xxx! xxx is a string with color 
-# 2. extend the function with another  input parameter 'color', that defaults to 'red'
-# 3. Capture the color via an input box as variable:color 
-# 4. Change the 'You are xx!' text to say 'you will be xx+1 years old next birthday 
-#  adding 1 to the age
-# 5. Capitalize first letter of the 'name', and rest are small caps 
-# 6. Favorite color should be in lowercase 
+# 1. Adicione uma nova declaração print em uma nova linha
+#    Que diga 'Você gosta da cor xxx! xxx é uma string com a cor.
+# 2. Estenda a função com outro parâmetro de entrada 'cor', com um vlaor default 'red'
+# 3. Capture a cor por meio de uma caixa de entrada, input como variável: cor
+# 4. Mude o texto 'você tem x anos' para você 'vai fazer x anos no proximo aniversário'. 
+#  adicionando 1 a idade
+# 5. Capitalize a primeira letra do nome.
+# 6. A cor favorita deve estar em caixa baixa.
