ae390043eb9320c382a928956d5237a8c7c172ac
laiananardi <lailai.santiago@gmail.com>
2020-11-26 10:23:35 -0300

    comparisons and conditionals if else elif

---
+++ courses/python_scrimba/16ComparisonsBooleans/index.py
@@ -0,0 +1,32 @@
+# a = [3,7,42]
+# b = a
+# print(a == b)
+# print(a is b)
+# print(id(a), id(b))
+
+# True
+# True
+# 1669869041152 1669869041152
+
+# a = [3,7,42]
+# b = [3,7,42]
+# print(a == b)
+# print(a is b)
+# print(id(a), id(b))
+
+# True
+# False
+# 2112481555904 2112481562944
+
+a=7
+b=3
+print('a == b is', a == b)
+print('a != b is', a != b)
+print('a > b is', a > b)
+print('a < b is', a < b)
+print('a >= b is', a >= b)
+print('a <= b is', a <= b)
+print('o in John is ','o' in 'John') #membership
+print('o in John is ','o' not in 'John') #non membership
+print('John is John ','John' is 'John') #identity
+print('John is not John is ','John' is not 'John') # negative identity
