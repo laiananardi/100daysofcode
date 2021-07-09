789775eba6f68a141afbcdb3051d40466489a578
laiananardi <lailai.santiago@gmail.com>
2021-07-09 06:05:56 -0300

    marble project starts

---
+++ courses/python_scrimba/34ProjectMarbleTrading game/index.py
@@ -0,0 +1,19 @@
+import random
+
+#create a bag with 10 marbles 
+bag = ('green','green','green','green','green','green','red','red','red','red')
+
+#starting amount of money
+#current money amount
+#result of last round
+#how many rounds
+#last marble
+# welcome user to game
+# loop drawing marbles
+    #how much was bet
+    #draw marble
+    # win or loss
+    #calc win or loss/ result and new amount/purse
+    #lose game if lose half of money
+    #print results
+# print final results
